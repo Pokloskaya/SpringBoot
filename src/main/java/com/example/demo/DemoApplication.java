ebbf2af2af92f8d190ecfd934ae0ab0f6db2940c
Julian Romero <master7n7@gmail.com>
2024-01-11 16:35:01 -0500

    done simple RESTful web service

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,18 +1,18 @@
 package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+@RestController //Steoreotype for RESTful controller
+@SpringBootApplication //meta annotation often placed on your main class (main class also know as configuration class)
+//@EnableAutoConfiguration tells Spring Boot how you want to configure Spring, based on the jar dependencies.
 
-@SpringBootApplication
-@RestController
 public class DemoApplication {
-    public static void main(String[] args) {
-      SpringApplication.run(DemoApplication.class, args);
-    }
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "defaultValue") String name) {
-      return String.format("Hello %s!", name);
-    }
+  @RequestMapping("/") //provides “routing” information. 
+  String home() {
+    return "Hello World!";
+  }
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 }
