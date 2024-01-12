0a0f05b758cb00d115d3893c5c83a95e28b0bfd8
Julian Romero <master7n7@gmail.com>
2024-01-12 17:25:35 -0500

    Feat: consuming a RESTful web service

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,16 +3,48 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+//------
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.client.RestTemplate;
+//------
+
 @RestController //Steoreotype for RESTful controller
 @SpringBootApplication //meta annotation often placed on your main class (main class also know as configuration class)
 //@EnableAutoConfiguration tells Spring Boot how you want to configure Spring, based on the jar dependencies.
 
 public class DemoApplication {
-  @RequestMapping("/") //provides “routing” information. 
+
+  private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+
+
+  @RequestMapping("/") 
   String home() {
-    return "Hello World!";
+    return "Esto es del primer tutorial de Spring Boot. Pero ya hice el segundo, wii";
   }
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
   }
+
+  @Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+  @Bean
+	@Profile("!test")
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject(
+					"http://localhost:8081/api/random", Quote.class);
+			log.info(quote.toString());
+		};
+	}
 }
---
+++ src/main/java/com/example/demo/Quote.java
@@ -0,0 +1,6 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true) //tells Jackson to ignore any attributes not listed in the class
+public record Quote(String type, Value value) { } //record is a new feature in Java 14. It is a class that is designed to store data.
